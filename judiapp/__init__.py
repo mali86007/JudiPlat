8ac4fcdc9c71d4595fc071f92907edf5417c03c0
mali86007 <malj007@tom.com>
2018-12-24 21:42:45 +0800

    user.login 2018.12.24H

--- judiapp/__init__.py
+++ judiapp/__init__.py
@@ -1,8 +1,8 @@
-import logging
+# import logging
 import os
-from logging.handlers import SMTPHandler, RotatingFileHandler
+# from logging.handlers import SMTPHandler, RotatingFileHandler
 
-import click
+# import click
 from flask import Flask, render_template, request
 from flask_login import current_user
 from flask_sqlalchemy import get_debug_queries
--- judiapp/extensions.py
+++ judiapp/extensions.py
@@ -10,13 +10,13 @@ from flask_migrate import Migrate
 
 bootstrap = Bootstrap()             # bootstrap实例
 db = SQLAlchemy()                   # 数据模型实例
-login_manager = LoginManager()
-csrf = CSRFProtect()
-ckeditor = CKEditor()
-mail = Mail()
-moment = Moment()
+login_manager = LoginManager()      # 用户管理实例
+csrf = CSRFProtect()                # CSRF令牌实例
+ckeditor = CKEditor()               # 富文本编辑器实例
+mail = Mail()                       # 电子邮箱实例
+moment = Moment()                   # 本地化实例
 # toolbar = DebugToolbarExtension()
-migrate = Migrate()
+migrate = Migrate()                 # 数据库迁移实例
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -24,5 +24,5 @@ def load_user(user_id):
     user = User.query.get(int(user_id))
     return user
 
-# login_manager.login_view = 'auth.login'
-# login_manager.login_message_category = 'warning'
+login_manager.login_view = 'user.login'
+login_manager.login_message_category = 'warning'
